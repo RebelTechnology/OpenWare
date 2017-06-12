70afc4dd0ae5c840499c6f66114cd279d3103bb7
mars <martin@rebel-it.com>
2017-06-12 17:27:30 +0100

    updated freertos and usb device configs

--- Tesseract/Src/main.c
+++ Tesseract/Src/main.c
@@ -495,7 +495,7 @@ static void MX_TIM2_Init(void)
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 100;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 1000;
+  htim2.Init.Period = 1023;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
   {
