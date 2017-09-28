f277b19c15ffb8cc478b76d49f526917846df518
collijp1 <collijp1@gmail.com>
2017-09-28 16:19:59 +0100

    TLC5971 driver completed and tested

--- QuadFM/Inc/TLC5971.h
+++ QuadFM/Inc/TLC5971.h
@@ -13,8 +13,8 @@
 #define LED_C				3
 #define LED_D				2
 		
-#define LED_Red			0
-#define LED_Green		1
+#define LED_Red			1
+#define LED_Green		0
 #define LED_Blue		2
  
 #define REG_BLANK		1<<0
@@ -26,7 +26,8 @@
 // Prototypes
 void TLC5971_init(SPI_HandleTypeDef *config);
 void TLC5971_SetOutput_GS(unsigned char LED_ID, unsigned char LED_Colour,  unsigned short value);
-void TLC5971_SetOutput_DC(unsigned char LED_ID, unsigned char LED_Colour,  unsigned char value);
+void TLC5971_SetOutput_BC(unsigned char LED_Colour, unsigned char value);
+void TLC5971_Settings(unsigned char value);
 void TLC5971_Update(void);
 
 #ifdef __cplusplus
