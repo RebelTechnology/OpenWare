0b6c1262a6a298a161fdb98e04ed97e357464634
mars <martin@rebel-it.com>
2017-10-03 11:55:39 +0100

    added GET_ARRAY service call and fast math tables

--- Source/ServiceCall.cpp
+++ Source/ServiceCall.cpp
@@ -1,8 +1,11 @@
 #include "arm_math.h"
 #include "arm_const_structs.h"
 #include "ServiceCall.h"
-// #include "ApplicationSettings.h"
+#include "ApplicationSettings.h"
 #include "OpenWareMidiControl.h"
+
+#include "FastLogTable.h"
+#include "FastPowTable.h"
 
 int SERVICE_ARM_CFFT_INIT_F32(arm_cfft_instance_f32* instance, int len){
   switch(len) { 
@@ -40,12 +43,13 @@ int SERVICE_ARM_CFFT_INIT_F32(arm_cfft_instance_f32* instance, int len){
 }
 
 int serviceCall(int service, void** params, int len){
+  int ret = OWL_SERVICE_INVALID_ARGS;
   switch(service){
   case OWL_SERVICE_VERSION:
     if(len > 0){
       int* value = (int*)params[0];
       *value = OWL_SERVICE_VERSION_V1;
-      return OWL_SERVICE_OK;
+      ret = OWL_SERVICE_OK;
     }
     break;
   case OWL_SERVICE_ARM_RFFT_FAST_INIT_F32:
@@ -53,37 +57,72 @@ int serviceCall(int service, void** params, int len){
       arm_rfft_fast_instance_f32* instance = (arm_rfft_fast_instance_f32*)params[0];
       int fftlen = *(int*)params[1];
       arm_rfft_fast_init_f32(instance, fftlen);
-      return OWL_SERVICE_OK;
+      ret = OWL_SERVICE_OK;
     }
     break;
   case OWL_SERVICE_ARM_CFFT_INIT_F32:
     if(len == 2){
       arm_cfft_instance_f32* instance = (arm_cfft_instance_f32*)params[0];
       int fftlen = *(int*)params[1];
-      return SERVICE_ARM_CFFT_INIT_F32(instance, fftlen);
+      ret = SERVICE_ARM_CFFT_INIT_F32(instance, fftlen);
     }
     break;
-  // case OWL_SERVICE_GET_PARAMETERS: {
-  //   int index = 0;
-  //   int ret = OWL_SERVICE_OK;
-  //   while(len >= index+2){
-  //     char* p = (char*)params[index++];
-  //     int32_t* value = (int32_t*)params[index++];
-  //     if(strncmp(SYSEX_CONFIGURATION_INPUT_OFFSET, p, 2) == 0){
-  // 	*value = settings.input_offset;
-  //     }else if(strncmp(SYSEX_CONFIGURATION_INPUT_SCALAR, p, 2) == 0){
-  // 	*value = settings.input_scalar;
-  //     }else if(strncmp(SYSEX_CONFIGURATION_OUTPUT_OFFSET, p, 2) == 0){
-  // 	*value = settings.output_offset;
-  //     }else if(strncmp(SYSEX_CONFIGURATION_OUTPUT_SCALAR, p, 2) == 0){
-  // 	*value = settings.output_scalar;
-  //     }else{
-  // 	ret = OWL_SERVICE_INVALID_ARGS;
-  //     }
-  //   }
-  //   return ret;
-  //   break;
-  // }
+  case OWL_SERVICE_GET_PARAMETERS: {
+    int index = 0;
+    ret = OWL_SERVICE_OK;
+    while(len >= index+2){
+      char* p = (char*)params[index++];
+      int32_t* value = (int32_t*)params[index++];
+      if(strncmp(SYSEX_CONFIGURATION_INPUT_OFFSET, p, 2) == 0){
+	*value = settings.input_offset;
+      }else if(strncmp(SYSEX_CONFIGURATION_INPUT_SCALAR, p, 2) == 0){
+	*value = settings.input_scalar;
+      }else if(strncmp(SYSEX_CONFIGURATION_OUTPUT_OFFSET, p, 2) == 0){
+	*value = settings.output_offset;
+      }else if(strncmp(SYSEX_CONFIGURATION_OUTPUT_SCALAR, p, 2) == 0){
+	*value = settings.output_scalar;
+      }else{
+	ret = OWL_SERVICE_INVALID_ARGS;
+      }
+    }
+    break;
   }
-  return OWL_SERVICE_INVALID_ARGS;
+  case OWL_SERVICE_GET_ARRAY: {
+    // get array and array size
+    // expects three parameters: name, &array and &size
+    int index = 0;
+    ret = OWL_SERVICE_OK;
+    if(len >= index+3){
+      char* p = (char*)params[index++];
+      void** array = (void**)params[index++];
+      int* size = (int*)params[index++];
+      if(strncmp(SYSTEM_TABLE_LOG, p, 3) == 0){
+	*array = (void*)fast_log_table;
+	*size = fast_log_table_size;
+      }else if(strncmp(SYSTEM_TABLE_POW, p, 3) == 0){
+	*array = (void*)fast_pow_table;
+	*size = fast_pow_table_size;
+      }else{
+	*array = NULL;
+	*size = 0;
+	ret = OWL_SERVICE_INVALID_ARGS;
+      }
+    }
+    // while(len >= index+2){
+    //   char* p = (char*)params[index++];
+    //   void** value = (void**)params[index++];
+    //   if(strncmp(SYSTEM_TABLE_ICSI_LOG, p, 3) == 0){
+    // 	*value = (void*)fast_log_table;
+    //   }else if(strncmp(SYSTEM_TABLE_ICSI_E_H, p, 3) == 0){
+    // 	*value = (void*)fast_pow_h_table;
+    //   }else if(strncmp(SYSTEM_TABLE_ICSI_E_L, p, 3) == 0){
+    // 	*value = (void*)fast_pow_l_table;
+    //   }else{
+    // 	ret = OWL_SERVICE_INVALID_ARGS;
+    //   }
+    // }
+    break;
+  }
+  }
+  return ret;
 }     
