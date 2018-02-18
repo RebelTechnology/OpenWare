0a9fc81e6091b6693eeae63f38409e54cfd0fd22
collijp1 <collijp1@gmail.com>
2018-02-18 14:35:25 +0000

    completed function to set LED colour and states

--- EffectsBox/Src/EffectsBox_Test.c
+++ EffectsBox/Src/EffectsBox_Test.c
@@ -6,15 +6,7 @@
 #include "Flash_S25FL.h"
 
 #define LED_RED	0
-#define LED_GRN	1
-
-#define SW1_LED(colour)	HAL_GPIO_WritePin(SW1_LED_GPIO_Port,  SW1_LED_Pin,  (GPIO_PinState)colour)
-#define SW2_LED(colour)	HAL_GPIO_WritePin(SW2_LED_GPIO_Port,  SW2_LED_Pin,  (GPIO_PinState)colour)
-#define SW3_LED(colour)	HAL_GPIO_WritePin(SW3_LED_GPIO_Port,  SW3_LED_Pin,  (GPIO_PinState)colour)
-#define SW4_LED(colour)	HAL_GPIO_WritePin(SW4_LED_GPIO_Port,  SW4_LED_Pin,  (GPIO_PinState)colour)
-#define SW5_LED(colour)	HAL_GPIO_WritePin(SW5_LED_GPIO_Port,  SW5_LED_Pin,  (GPIO_PinState)colour)
-#define SW6_LED(colour)	HAL_GPIO_WritePin(SW6_LED_GPIO_Port,  SW6_LED_Pin,  (GPIO_PinState)colour)
-#define SW7_LED(colour)	HAL_GPIO_WritePin(SW7_LED_GPIO_Port,  SW7_LED_Pin,  (GPIO_PinState)colour)
+#define LED_YEL	1
 
 #define SW1_Read()		(1-(uint8_t)HAL_GPIO_ReadPin(SW1_BTN_GPIO_Port,  SW1_BTN_Pin))
 #define SW2_Read()		(1-(uint8_t)HAL_GPIO_ReadPin(SW2_BTN_GPIO_Port,  SW2_BTN_Pin))
@@ -28,28 +20,17 @@
 #define TSW2_Read()		(1-HAL_GPIO_ReadPin(TSW2_A_GPIO_Port,  TSW2_A_Pin)) | (1-HAL_GPIO_ReadPin(TSW2_B_GPIO_Port,  TSW2_B_Pin))<<1
 
 extern SPI_HandleTypeDef hspi5;
-	
-uint8_t TSW1_State;
-uint8_t TSW2_State;
+uint8_t TSW1_State,TSW2_State;
 
-enum {
-	YELLOW, RED, NONE
-} LedColour;
-
-void setLed(uint8_t led, LedColour colour)
-{
-
-}
-
-void SW_LED_EN(uint8_t sw, uint8_t state)
+void setLED(uint8_t led, LEDcolour col)
 {
 	GPIO_InitTypeDef 	GPIO_InitStruct;
 	GPIO_TypeDef* 		GPIOx; 
 	uint16_t 					GPIO_Pin;
+	uint8_t 					LED_Colour;
 	
 	// Get switch pin and port number
-	switch(sw)
-	{
+	switch(led){
 		case 1: GPIOx = SW1_LED_GPIO_Port;	GPIO_Pin = SW1_LED_Pin; break;
 		case 2: GPIOx = SW2_LED_GPIO_Port;	GPIO_Pin = SW2_LED_Pin; break;
 		case 3: GPIOx = SW3_LED_GPIO_Port;	GPIO_Pin = SW3_LED_Pin; break;
@@ -63,11 +44,17 @@ void SW_LED_EN(uint8_t sw, uint8_t state)
 	GPIO_InitStruct.Pin = GPIO_Pin;
 	GPIO_InitStruct.Pull = GPIO_NOPULL;
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	if (state){GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;}	// Enable
-	else			{GPIO_InitStruct.Mode = GPIO_MODE_INPUT;}			// Disable
-
-	// Update Pin
+	
+	// Set Output direction and LED colour
+	switch (col){
+		case YELLOW:	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;	LED_Colour = LED_YEL; break;
+		case RED: 		GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP; LED_Colour = LED_RED; break;
+		case NONE: 		GPIO_InitStruct.Mode = GPIO_MODE_INPUT;														break;
+	}
+	
+	// Update Pin	
 	HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);
+	HAL_GPIO_WritePin(GPIOx,  GPIO_Pin,  (GPIO_PinState)LED_Colour);
 }
 
 void EffectsBox_Init(void)
@@ -95,27 +82,42 @@ void EffectsBox_Init(void)
 
 void EffectsBox_Main(void)
 {
-
 	if(TSW1_Read())
 	{
-		SW_LED_EN(1, !SW1_Read());
-		SW_LED_EN(2, !SW2_Read());
-		SW_LED_EN(3, !SW3_Read());
-		SW_LED_EN(4, !SW4_Read());
-		SW_LED_EN(5, !SW5_Read());
-		SW_LED_EN(6, !SW6_Read());
-		SW_LED_EN(7, !SW7_Read());
-	}
+		if (SW1_Read()) {setLED(1, RED);}
+		else						{setLED(1, YELLOW);}
+		
+		if (SW2_Read()) {setLED(2, RED);}
+		else						{setLED(2, YELLOW);}
+		
+		if (SW3_Read()) {setLED(3, RED);}
+		else						{setLED(3, YELLOW);}
+		
+		if (SW4_Read()) {setLED(4, RED);}
+		else						{setLED(4, YELLOW);}
+		
+		if (SW5_Read()) {setLED(5, RED);}
+		else						{setLED(5, YELLOW);}
+		
+		if (SW6_Read()) {setLED(6, RED);}
+		else						{setLED(6, YELLOW);}
+		
+		if (SW7_Read()) {setLED(7, RED);}
+		else						{setLED(7, YELLOW);}
 	
-	SW1_LED(SW1_Read());
-	SW2_LED(SW2_Read());
-	SW3_LED(SW3_Read());
-	SW4_LED(SW4_Read());
-	SW5_LED(SW5_Read());
-	SW6_LED(SW6_Read());
-	SW7_LED(SW7_Read());
+	}
+	else
+	{
+		setLED(1, NONE);
+		setLED(2, NONE);
+		setLED(3, NONE);
+		setLED(4, NONE);
+		setLED(5, NONE);
+		setLED(6, NONE);
+		setLED(7, NONE);
+	}
 
-//	TSW2_State = TSW2_Read();
+	TSW2_State = TSW2_Read();
 
 	OLED_Refresh();
 }
