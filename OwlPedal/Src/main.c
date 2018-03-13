4f7053aa462db93966bf4329f07bc5b9b375c035
mars <martin@rebel-it.com>
2018-03-13 14:18:40 +0000

    changed default stack size

--- OwlPedal/Src/main.c
+++ OwlPedal/Src/main.c
@@ -238,7 +238,7 @@ int main(void)
 
   /* Create the thread(s) */
   /* definition and creation of defaultTask */
-  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
+  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 512);
   defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
 
   /* USER CODE BEGIN RTOS_THREADS */
