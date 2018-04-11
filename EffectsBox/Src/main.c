2267d6e75875519b9686de1b267182dd100539b8
mars <martin@rebel-it.com>
2018-04-11 10:26:55 +0100

    changed rtos heap and task size

--- EffectsBox/Src/main.c
+++ EffectsBox/Src/main.c
@@ -202,7 +202,7 @@ int main(void)
 
   /* Create the thread(s) */
   /* definition and creation of defaultTask */
-  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 768);
+  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 1024);
   defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
 
   /* USER CODE BEGIN RTOS_THREADS */
