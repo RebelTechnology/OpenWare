c32f1a6294daed48f0fbfca408c757903452ee67
Martin Klang <martin@rebel-it.com>
2018-09-18 17:30:24 +0100

    enabled SWD

--- Alchemist/Src/main.c
+++ Alchemist/Src/main.c
@@ -699,11 +699,9 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PA4 PA5 PA8 PA9 
-                           PA10 PA11 PA12 PA13 
-                           PA14 PA15 */
+                           PA10 PA11 PA12 PA15 */
   GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_8|GPIO_PIN_9 
-                          |GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13 
-                          |GPIO_PIN_14|GPIO_PIN_15;
+                          |GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_15;
   GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
