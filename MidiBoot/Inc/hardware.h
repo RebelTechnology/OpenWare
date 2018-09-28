e544532bd93729a45496c4b6abdcdc6a174c301f
Martin Klang <martin@rebel-it.com>
2018-09-28 11:33:12 +0100

    config tidy

--- MidiBoot/Inc/hardware.h
+++ MidiBoot/Inc/hardware.h
@@ -2,9 +2,9 @@
 #include "stm32f4xx_hal.h"
 
 #define OWL_BOOT
-#define USE_USBD_HS
 
-// #define USE_BOOT1_PIN
+#define USE_USBD_HS
+#define USE_BOOT1_PIN
 
 #define APPLICATION_ADDRESS 0x08010000
 
