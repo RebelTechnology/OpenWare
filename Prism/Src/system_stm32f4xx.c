cb7c95ab0df55b5c390a5cdf4565c3104944da14
Martin Klang <martin@rebel-it.com>
2018-11-01 17:19:14 +0000

    updated to work without bootloader

--- Prism/Src/system_stm32f4xx.c
+++ Prism/Src/system_stm32f4xx.c
@@ -106,7 +106,8 @@
 /*!< Uncomment the following line if you need to relocate your vector Table in
      Internal SRAM. */
 /* #define VECT_TAB_SRAM */
-#define VECT_TAB_OFFSET  0x00010000 /*!< Vector Table base offset field. 
+/* #define VECT_TAB_OFFSET  0x00010000 // used with MIDI bootloader */
+#define VECT_TAB_OFFSET  0x00000000 /*!< Vector Table base offset field. 
                                    This value must be a multiple of 0x200. */
 /******************************************************************************/
 
