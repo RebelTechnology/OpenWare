f662cf38bff7ca568305e7f7e3678f36cb65989b
Martin Klang <martin@rebel-it.com>
2018-11-19 14:26:49 +0000

    updates for Wavetable prototype

--- Wavetable/Inc/hardware.h
+++ Wavetable/Inc/hardware.h
@@ -4,17 +4,20 @@
 
 /* #define OWL_TESSERACT_REV4 // SWO mapped to toggle */
 
-#define OWL_TESSERACT
+#define OWL_WAVETABLE
 #define HARDWARE_ID                  WAVETABLE_HARDWARE
 #define HARDWARE_VERSION             "Wavetable Prototype"
 
 #define USE_RGB_LED
 #define USE_ADC
-#define ADC_PERIPH hadc3
-#define ADC_A 1
-#define ADC_B 3
-#define ADC_C 2
-#define ADC_D 0
+#define ADC_A 5
+#define ADC_B 0
+#define ADC_C 3
+#define ADC_D 1
+#define ADC_E 6
+#define ADC_F 4
+#define ADC_G 2
+#define ADC_H 7
 #define USE_CS4271
 #define USE_USBD_FS
 #define USE_CODEC
