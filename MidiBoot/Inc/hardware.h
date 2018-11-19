55bdf893ea5d4a450267a32096c387d154852a18
Martin Klang <martin@rebel-it.com>
2018-11-19 14:51:07 +0000

    configured build

--- MidiBoot/Inc/hardware.h
+++ MidiBoot/Inc/hardware.h
@@ -1,21 +1,28 @@
 #include "main.h"
 #include "stm32f4xx_hal.h"
 
-#define OWL_BOOT
 #define HARDWARE_VERSION             "OWL MIDI Boot"
 
-/* #define OWL_MAGUS */
-
-#ifdef OWL_MAGUS
-/* Magus */
-#define USE_USBD_FS
-#undef USE_BOOT1_PIN
+#if defined OWL_MAGUS
+  #define USE_USBD_FS
+  #define APPLICATION_ADDRESS 0x08010000
+#elif defined OWL_ALCHEMIST || defined OWL_WIZARD
+  #define USE_USBD_HS
+  #define USE_BOOT1_PIN
+  #define APPLICATION_ADDRESS 0x08010000
+#elif defined OWL_WAVETABLE
+  #define USE_USBD_FS
+  #define APPLICATION_ADDRESS 0x08010000
+#elif defined OWL_PEDAL
+  #define USE_USBD_HS
+  #define APPLICATION_ADDRESS 0x08008000
+  #define USE_BOOT1_PIN
+#elif defined OWL_RACK
+  #define USE_USBD_HS
+  #define APPLICATION_ADDRESS 0x08008000
 #else
-/* Alchemist and Wizard */
-#define USE_USBD_HS
-#define USE_BOOT1_PIN
+  #error Invalid configuration
 #endif
 
-#define APPLICATION_ADDRESS 0x08010000
 
 /* #define INIT_FMC */
