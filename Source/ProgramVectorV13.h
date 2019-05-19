660df7cc4a6509380a121d9ffff2cf1de68fc9d4
Martin <mars@pingdynasty.com>
2019-05-19 19:14:43 +0200

    updated for Noctua hardware

--- Source/ProgramVectorV13.h
+++ Source/ProgramVectorV13.h
@@ -21,6 +21,7 @@
 #define MAGUS_HARDWARE              0x19
 #define EFFECTSBOX_HARDWARE         0x1a
 #define WAVETABLE_HARDWARE          0x1b
+#define NOCTUA_HARDWARE             0x1c
 
 #define PROGRAM_VECTOR_CHECKSUM_V11 0x40
 #define PROGRAM_VECTOR_CHECKSUM_V12 0x50
