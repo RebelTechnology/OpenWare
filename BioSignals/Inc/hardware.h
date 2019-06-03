eab77d18ed75a006b39ef744774662cb441ffeae
Martin <mars@pingdynasty.com>
2019-06-03 11:03:52 +0200

    cleaned up config

--- BioSignals/Inc/hardware.h
+++ BioSignals/Inc/hardware.h
@@ -11,32 +11,24 @@
 #define AUDIO_BYPASS
 
 #define USE_KX122
-/* #define KX122_LEFTSHIFT               16 */
 #define KX122_ACTIVE_CHANNELS         3
 /* #define KX122_AUDIO_FREQ              25600 */
 /* #define KX122_AUDIO_FREQ              12800 */
 #define KX122_AUDIO_FREQ              6400
 #define KX122_TOTAL_CHANNELS          3 /* Always 3 */
-#define KX122_BLOCKSIZE               1 /* Always 1 */
-#define KX122_BUFFER_SIZE             (KX122_TOTAL_CHANNELS*KX122_BLOCKSIZE)
-#define KX122_RINGBUFFER_SIZE         (KX122_ACTIVE_CHANNELS*128)
-#define KX122_RINGBUFFER_OVER_LIMIT   (2*KX122_RINGBUFFER_SIZE/3)
-#define KX122_RINGBUFFER_UNDER_LIMIT  (KX122_RINGBUFFER_SIZE/3)
 #define KX122_HSPI                    hspi3
 
 #define ADS_AUDIO_FREQ              8000
-#define ADS_GAIN                    1
+#define ADS_GAIN                    4
 #define ADS_MAX_CHANNELS            4
 #define ADS_ACTIVE_CHANNELS         4
 #define ADS_HSPI                    hspi1
+
 #define AUDIO_RINGBUFFER_SIZE       (CODEC_BLOCKSIZE*USB_AUDIO_CHANNELS*4)
 
 /* USB audio settings */
 #define AUDIO_BITS_PER_SAMPLE       32
 #define AUDIO_BYTES_PER_SAMPLE      (AUDIO_BITS_PER_SAMPLE/8)
-/* #define ADC_CHANNEL_OFFSET          0 */
-/* #define ADC_RIGHTSHIFT              0 */
-
 #define USBD_AUDIO_FREQ             8000
 
 #define TIM8_PERIOD                 (871*48000/USBD_AUDIO_FREQ) /* experimentally determined */
