9e8ebe31a2426eb752942a14b9eb2de274e10519
Martin <mars@pingdynasty.com>
2019-07-19 19:34:23 +0200

    added Noctua definition

--- MidiBoot/Inc/hardware.h
+++ MidiBoot/Inc/hardware.h
@@ -16,6 +16,10 @@
   #define USE_USBD_HS
   #define USE_BOOT1_PIN
   #define APPLICATION_ADDRESS 0x08010000
+#elif defined OWL_NOCTUA
+  #define HARDWARE_VERSION    "OWL Noctua Boot"
+  #define USE_USBD_FS
+  #define APPLICATION_ADDRESS 0x08010000
 #elif defined OWL_WAVETABLE
   #define HARDWARE_VERSION    "OWL WaveTable Boot"
   #define USE_USBD_FS
