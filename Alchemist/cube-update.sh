88d9c23c2a7026f6e11394245503183d8f6804b0
Martin <mars@pingdynasty.com>
2019-12-23 00:53:35 +0100

    revert Makefile

--- Alchemist/cube-update.sh
+++ Alchemist/cube-update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # git checkout Src/usbd_desc.c 
+git checkout Makefile
 git checkout Src/usb_device.c 
 # git checkout Inc/FreeRTOSConfig.h Middlewares/Third_Party/FreeRTOS
 # git checkout Src/usb_host.c Inc/usb_host.h 
--- Wizard/cube-update.sh
+++ Wizard/cube-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# git checkout Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/
-mv Src/usb_device.c Src/usbd_conf.c Src/usbd_desc.c Src/usbh_conf.c usbd-hs/
+git checkout Makefile
 git checkout Inc/usbd_desc.h
 git checkout Src/stm32f4xx_it.c
+mv Src/usb_device.c Src/usbd_conf.c Src/usbd_desc.c Src/usbh_conf.c usbd-hs/
 rm -f Src/usbd_audio_if.c Inc/usbd_audio_if.h
 rm -f Src/*.tmp
