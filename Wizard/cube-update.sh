7efef0feeebff1bb367df671afb31d07d10d5e92
Martin <mars@pingdynasty.com>
2020-03-31 21:22:07 +0200

    cube updates / enable USB HS DMA

--- Wizard/cube-update.sh
+++ Wizard/cube-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git checkout Makefile
-git checkout Inc/usbd_desc.h
+git checkout Inc/usbd_desc.h Inc/usbh_conf.h
 git checkout Src/stm32f4xx_it.c
+git checkout Src/usb_host.c
 mv Src/usb_device.c Src/usbd_conf.c Src/usbd_desc.c Src/usbh_conf.c usbd-hs/
 rm -f Src/usbd_audio_if.c Inc/usbd_audio_if.h
-rm -f Src/*.tmp
--- Wizard/usbd-hs/usbd_conf.c
+++ Wizard/usbd-hs/usbd_conf.c
@@ -341,7 +341,7 @@ USBD_StatusTypeDef USBD_LL_Init(USBD_HandleTypeDef *pdev)
   hpcd_USB_OTG_HS.Instance = USB_OTG_HS;
   hpcd_USB_OTG_HS.Init.dev_endpoints = 6;
   hpcd_USB_OTG_HS.Init.speed = PCD_SPEED_FULL;
-  hpcd_USB_OTG_HS.Init.dma_enable = DISABLE;
+  hpcd_USB_OTG_HS.Init.dma_enable = ENABLE;
   hpcd_USB_OTG_HS.Init.phy_itface = USB_OTG_EMBEDDED_PHY;
   hpcd_USB_OTG_HS.Init.Sof_enable = DISABLE;
   hpcd_USB_OTG_HS.Init.low_power_enable = DISABLE;
