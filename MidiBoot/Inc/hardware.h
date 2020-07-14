fd07a7a896d5f607b0aa5d3df3a004bc72a25d5f
Stanislav Shtin <antisvin@gmail.com>
2020-07-14 21:40:39 +0300

    Original midiboot doesnt's support owl1, no need to have settings for it

--- MidiBoot/Inc/hardware.h
+++ MidiBoot/Inc/hardware.h
@@ -28,14 +28,6 @@
 #elif defined OWL_WAVETABLE
   #define HARDWARE_VERSION    "OWL WaveTable Boot"
   #define APPLICATION_ADDRESS 0x08010000
-#elif defined OWL_PEDAL
-  #define HARDWARE_VERSION    "OWL Pedal Boot"
-  #define APPLICATION_ADDRESS 0x08008000
-  #define USE_BOOT1_PIN
-  #define USE_LED
-#elif defined OWL_RACK
-  #define HARDWARE_VERSION    "OWL Rack Boot"
-  #define APPLICATION_ADDRESS 0x08008000
 #elif defined OWL_BIOSIGNALS
   #define HARDWARE_VERSION    "OWL BioSignals Boot"
   #define APPLICATION_ADDRESS 0x08010000
