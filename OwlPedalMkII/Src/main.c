62b709a4d692173b1785b3183801b62aac5b1d00
Martin Klang <martin@rebel-it.com>
2020-10-13 17:23:07 +0200

    configure rising/falling interrupt

--- OwlPedalMkII/Src/main.c
+++ OwlPedalMkII/Src/main.c
@@ -732,7 +732,7 @@ static void MX_GPIO_Init(void)
 
   /*Configure GPIO pins : SW4_Pin SW3_Pin SW1_Pin */
   GPIO_InitStruct.Pin = SW4_Pin|SW3_Pin|SW1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
