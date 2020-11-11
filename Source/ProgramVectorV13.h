153da48dc807bb7e73322ff305cbaafffc05fc56
Martin Klang <martin@rebel-it.com>
2020-11-11 19:22:51 +0100

    hardware id for Genius

--- Source/ProgramVectorV13.h
+++ Source/ProgramVectorV13.h
@@ -11,6 +11,23 @@
  extern "C" {
 #endif
 
+#define OWL_PEDAL_HARDWARE          0x11
+#define OWL_MODULAR_HARDWARE        0x12
+#define OWL_RACK_HARDWARE           0x13
+#define PRISM_HARDWARE              0x14
+#define PLAYER_HARDWARE             0x15
+#define TESSERACT_HARDWARE          0x16
+#define ALCHEMIST_HARDWARE          0x17
+#define WIZARD_HARDWARE             0x18
+#define MAGUS_HARDWARE              0x19
+#define EFFECTSBOX_HARDWARE         0x1a
+#define WAVETABLE_HARDWARE          0x1b
+#define NOCTUA_HARDWARE             0x1c
+#define BIOSIGNALS_HARDWARE         0x1d
+#define LICH_HARDWARE               0x1e
+#define WITCH_HARDWARE              0x1f
+#define GENIUS_HARDWARE             0x20
+
 #define PROGRAM_VECTOR_CHECKSUM_V11 0x40
 #define PROGRAM_VECTOR_CHECKSUM_V12 0x50
 #define PROGRAM_VECTOR_CHECKSUM_V13 0x51
