ea3822678cc25c4c9cfdd61f1bc1df0d19d7a693
Martin Klang <martin@rebel-it.com>
2020-11-25 22:26:45 +0100

    adapted to usbd updates

--- Magus/Inc/hardware.h
+++ Magus/Inc/hardware.h
@@ -20,35 +20,34 @@
 #define AUDIO_BITS_PER_SAMPLE       16
 #define AUDIO_BYTES_PER_SAMPLE      (AUDIO_BITS_PER_SAMPLE/8)
 #define AUDIO_CHANNELS              2
-#define USB_AUDIO_CHANNELS          2
-/* #define AUDIO_INT32_TO_SAMPLE(x)    (__REV16((x)>>8)) */
-/* #define AUDIO_SAMPLE_TO_INT32(x)    ((int32_t)(__REV16(x))<<8) */
+#define USBD_AUDIO_RX_CHANNELS      AUDIO_CHANNELS
+#define USBD_AUDIO_TX_CHANNELS      AUDIO_CHANNELS
 #define AUDIO_INT32_TO_SAMPLE(x)    ((x)>>8)
 #define AUDIO_SAMPLE_TO_INT32(x)    ((int32_t)(x)<<8)
 
 #define MAIN_LOOP_SLEEP_MS          20
 
 #define USE_USBD_AUDIO
-#define USE_USBD_AUDIO_TX  // microphone
-/* #define USE_USBD_AUDIO_RX // speaker */
+#define USE_USBD_AUDIO_TX           // microphone
+#define USE_USBD_AUDIO_RX           // speaker
 #define USE_USBD_FS
-#define USBD_HANDLE hUsbDeviceFS
-#define USBH_HANDLE hUsbHostHS
+#define USBD_HANDLE                 hUsbDeviceFS
+#define USBH_HANDLE                 hUsbHostHS
 #define USE_USB_HOST
-#define USB_HOST_RX_BUFF_SIZE 256  /* Max Received data 64 bytes */
 #define USE_USBH_MIDI
+#define USB_HOST_RX_BUFF_SIZE       256
 
 #define TLC5946_SPI hspi6
 #define MAX11300_SPI hspi5
 #define ENCODERS_SPI hspi5
 
 #undef USE_ADC
-#define NOF_ADC_VALUES               0
-#define NOF_PARAMETERS               20
-#define NOF_BUTTONS                  0
+#define NOF_ADC_VALUES              0
+#define NOF_PARAMETERS              20
+#define NOF_BUTTONS                 0
 #undef USE_DAC
 
-#define AUDIO_INPUT_OFFSET           ((uint32_t)(0.00007896*65535))
-#define AUDIO_INPUT_SCALAR           ((uint32_t)(-6.43010423*65535))
-#define AUDIO_OUTPUT_OFFSET          ((uint32_t)(0.00039729*65535))
-#define AUDIO_OUTPUT_SCALAR          ((uint32_t)(5.03400000*65535))
+#define AUDIO_INPUT_OFFSET          ((uint32_t)(0.00007896*65535))
+#define AUDIO_INPUT_SCALAR          ((uint32_t)(-6.43010423*65535))
+#define AUDIO_OUTPUT_OFFSET         ((uint32_t)(0.00039729*65535))
+#define AUDIO_OUTPUT_SCALAR         ((uint32_t)(5.03400000*65535))
