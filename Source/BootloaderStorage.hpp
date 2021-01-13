da5c17151afd422b0177e2cb3f879a27911c3732
antisvin <antisvin@gmail.com>
2021-01-13 23:12:12 +0300

    Determine section to delete based on bootloader size

--- Source/BootloaderStorage.hpp
+++ Source/BootloaderStorage.hpp
@@ -4,18 +4,20 @@
 #include <inttypes.h>
 #include "eepromcontrol.h"
 
-extern char _BOOTLOADER;
+extern char _BOOTLOADER, _BOOTLOADER_END;
 
 class BootloaderStorage {
 public:
     bool erase(){
         eeprom_unlock();
+        // We expect bootloader to occupy 1 - 4 sectors. Currently OWL1 uses 2 sectors, OWL2 uses 4.
         int ret = eeprom_erase_sector(FLASH_SECTOR_0);
-        ret |= eeprom_erase_sector(FLASH_SECTOR_1);
-#if !defined(OWL_MODULAR) && !defined(OWL_PEDAL)
-        ret |= eeprom_erase_sector(FLASH_SECTOR_2);
-        ret |= eeprom_erase_sector(FLASH_SECTOR_3);
-#endif
+        if (ADDR_FLASH_SECTOR_1 < (uint32_t)&_BOOTLOADER_END)
+            ret |= eeprom_erase_sector(FLASH_SECTOR_1);
+        if (ADDR_FLASH_SECTOR_2 < (uint32_t)&_BOOTLOADER_END)
+            ret |= eeprom_erase_sector(FLASH_SECTOR_2);
+        if (ADDR_FLASH_SECTOR_3 < (uint32_t)&_BOOTLOADER_END)
+            ret |= eeprom_erase_sector(FLASH_SECTOR_3);
         eeprom_lock();
         return ret == 0;
     }
