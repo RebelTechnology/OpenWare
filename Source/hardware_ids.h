58a6615021be09fddc020d2735a405ec03dfa141
Martin Klang <martin@rebel-it.com>
2021-01-20 11:47:32 +0100

    added Genius id

--- Source/hardware_ids.h
+++ Source/hardware_ids.h
@@ -1,3 +1,6 @@
+#ifndef __HARDWARE_IDS_H
+#define __HARDWARE_IDS_H
+
 #define OWL_PEDAL_HARDWARE          0x11
 #define OWL_MODULAR_HARDWARE        0x12
 #define OWL_RACK_HARDWARE           0x13
@@ -13,3 +16,6 @@
 #define BIOSIGNALS_HARDWARE         0x1d
 #define LICH_HARDWARE               0x1e
 #define WITCH_HARDWARE              0x1f
+#define GENIUS_HARDWARE             0x20
+
+#endif /* __HARDWARE_IDS_H */
