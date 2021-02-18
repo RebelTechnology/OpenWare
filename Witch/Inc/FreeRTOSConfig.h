c222d408ce7b54e031758126a9375b05747ab474
Martin Klang <martin@rebel-it.com>
2021-02-18 15:17:57 +0100

    adjusted task heap allocations

--- Witch/Inc/FreeRTOSConfig.h
+++ Witch/Inc/FreeRTOSConfig.h
@@ -112,7 +112,7 @@
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
 #define configMAX_PRIORITIES                     ( 7 )
 #define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
-#define configTOTAL_HEAP_SIZE                    ((size_t)8192)
+#define configTOTAL_HEAP_SIZE                    ((size_t)4096)
 #define configMAX_TASK_NAME_LEN                  ( 16 )
 #define configUSE_16_BIT_TICKS                   0
 #define configQUEUE_REGISTRY_SIZE                8
