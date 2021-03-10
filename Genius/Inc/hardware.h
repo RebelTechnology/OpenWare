4653d3ff5790130471c6240fe5ecb357e2ab0431
antisvin <antisvin@gmail.com>
2021-03-10 11:00:56 +0300

    Cache support

--- Genius/Inc/hardware.h
+++ Genius/Inc/hardware.h
@@ -8,9 +8,11 @@
 /* #define NO_EXT_RAM */
 /* #define NO_CCM_RAM */
 #define DMA_RAM                      __attribute__ ((section (".dmadata")))
+#define CACHE_ALIGNED                __attribute__ ((aligned(32)))
+
 #define USE_PLUS_RAM
-/* #define USE_ICACHE */
-/* #define USE_DCACHE */
+#define USE_ICACHE
+#define USE_DCACHE
 
 #define MAX_SYSEX_PROGRAM_SIZE      (512*1024)
 
