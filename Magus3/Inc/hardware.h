3bcb27bf6b257640fee9216f1d0231c891538f8d
Martin Klang <martin@rebel-it.com>
2021-03-22 14:08:26 +0100

    draft version of Magus with OWL3

---
+++ Magus3/Inc/hardware.h
@@ -0,0 +1,56 @@
+#include "main.h"
+#include "stm32f4xx_hal.h"
+
+#define OWL_MAGUS
+#define HARDWARE_ID                  MAGUS_HARDWARE
+#define HARDWARE_VERSION             "Magus"
+
+#define USE_SCREEN
+#define SSD1309
+/* #define OLED_DMA */
+#define OLED_SOFT_CS
+#define OLED_SPI hspi6
+/* #define OLED_IT */
+/* #define OLED_BITBANG */
+#define USE_CODEC
+#define USE_CS4271
+#define CODEC_SPI hspi4
+
+/* USB audio settings */
+#define AUDIO_BITS_PER_SAMPLE       16
+#define AUDIO_BYTES_PER_SAMPLE      (AUDIO_BITS_PER_SAMPLE/8)
+#define AUDIO_CHANNELS              2
+#define AUDIO_INT32_TO_SAMPLE(x)    ((x)>>8)
+#define AUDIO_SAMPLE_TO_INT32(x)    ((int32_t)(x)<<8)
+
+#define MAIN_LOOP_SLEEP_MS          20
+
+#define USE_USBD_AUDIO
+#define USE_USBD_AUDIO_TX           // microphone
+/* #define USE_USBD_AUDIO_RX           // speaker */
+#define USE_USBD_FS
+#define USBD_HANDLE                 hUsbDeviceFS
+#define USBH_HANDLE                 hUsbHostHS
+#define USE_USB_HOST
+#define USE_USBH_MIDI
+#define USB_HOST_RX_BUFF_SIZE       256
+
+// Serial MIDI TX only (or digital bus with an extra pin)
+#define USE_UART_MIDI
+#define UART_MIDI_HANDLE huart2
+#define UART_MIDI_RX_BUFFER_SIZE 256
+
+#define TLC5946_SPI hspi6
+#define MAX11300_SPI hspi5
+#define ENCODERS_SPI hspi5
+
+#undef USE_ADC
+#define NOF_ADC_VALUES              0
+#define NOF_PARAMETERS              20
+#define NOF_BUTTONS                 0
+#undef USE_DAC
+
+#define AUDIO_INPUT_OFFSET          ((uint32_t)(0.00007896*65535))
+#define AUDIO_INPUT_SCALAR          ((uint32_t)(-6.43010423*65535))
+#define AUDIO_OUTPUT_OFFSET         ((uint32_t)(0.00039729*65535))
+#define AUDIO_OUTPUT_SCALAR         ((uint32_t)(5.03400000*65535))
---
+++ Magus3/Src/Magus.cpp
@@ -0,0 +1,183 @@
+#include "Owl.h"
+#include "device.h"
+#include "errorhandlers.h"
+#include "Graphics.h"
+#include "purple-blue-cyan.h"
+#include "orange-red-pink.h"
+#include "HAL_TLC5946.h"
+#include "HAL_MAX11300.h"
+#include "HAL_Encoders.h"
+
+#define TLC5940_RED_DC 0x55
+#define TLC5940_GREEN_DC 0x55
+#define TLC5940_BLUE_DC 0x55
+
+const uint32_t* dyn_rainbowinputs = rainbowinputs;
+const uint32_t* dyn_rainbowoutputs = rainbowoutputs;
+Graphics graphics;
+
+extern "C" void onResourceUpdate(void);
+
+static bool updateMAX11300 = false;
+static uint8_t portMode[20];
+void setPortMode(uint8_t index, uint8_t mode){
+  // todo: select range automatically based on output value
+  if(index < 20){
+    if(portMode[index] != mode){
+      portMode[index] = mode;
+      updateMAX11300 = true;
+      // MAX11300_setDACValue(index+1, 0);
+    }
+  }
+}
+uint8_t getPortMode(uint8_t index){
+  if(index < 20)
+    return portMode[index];
+  return PORT_UNI_INPUT;
+}
+
+void setLed(uint8_t led, uint32_t rgb){
+  TLC5946_setRGB(led+1, ((rgb>>20)&0x3ff)<<2, ((rgb>>10)&0x3ff)<<2, ((rgb>>00)&0x3ff)<<2);
+}
+
+void onResourceUpdate(void){
+  extern const uint32_t rainbowinputs[];
+  extern const uint32_t rainbowoutputs[];
+  extern const uint32_t* dyn_rainbowinputs;
+  extern const uint32_t* dyn_rainbowoutputs;
+  ResourceHeader* res = registry.getResource("Rainbow.in");
+  if (res == NULL){
+    dyn_rainbowinputs = rainbowinputs;
+  }
+  else {
+    dyn_rainbowinputs = (uint32_t*)registry.getData(res);
+  }
+  res = registry.getResource("Rainbow.out");
+  if (res == NULL){
+    dyn_rainbowoutputs = rainbowoutputs;
+  }
+  else {
+    dyn_rainbowoutputs = (uint32_t*)registry.getData(res);
+  }
+}
+
+void setup(){
+  HAL_GPIO_WritePin(TLC_BLANK_GPIO_Port, TLC_BLANK_Pin, GPIO_PIN_SET); // LEDs off
+  HAL_GPIO_WritePin(ENC_NRST_GPIO_Port, ENC_NRST_Pin, GPIO_PIN_RESET); // Reset encoders 
+  {
+    extern SPI_HandleTypeDef TLC5946_SPI;
+
+    // LEDs
+    TLC5946_init(&TLC5946_SPI);
+    // TLC5946_setRGB_DC(63, 19, 60); // TODO: balance levels
+    TLC5946_setRGB_DC(TLC5940_RED_DC, TLC5940_GREEN_DC, TLC5940_BLUE_DC);
+    TLC5946_setAll(0x10, 0x10, 0x10);
+
+    HAL_GPIO_WritePin(TLC_BLANK_GPIO_Port, TLC_BLANK_Pin, GPIO_PIN_RESET);
+
+    TLC5946_Refresh_DC();
+    TLC5946_Refresh_GS();
+    HAL_Delay(100);
+
+    // Start BLANK PWM
+    extern TIM_HandleTypeDef htim2;
+    HAL_TIM_Base_Start(&htim2);
+    HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+
+    // Start LED Driver PWM : GSCLK
+    extern TIM_HandleTypeDef htim3;
+    HAL_TIM_Base_Start(&htim3);
+    HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_4);
+  }
+  {
+    // Encoders
+    extern SPI_HandleTypeDef ENCODERS_SPI;
+    Encoders_init(&ENCODERS_SPI);
+    Encoders_readAll();
+  }
+  {
+    // Pixi
+    extern SPI_HandleTypeDef MAX11300_SPI;
+    MAX11300_init(&MAX11300_SPI);
+    MAX11300_setDeviceControl(DCR_RESET);
+  }
+
+  HAL_GPIO_WritePin(OLED_RST_GPIO_Port, OLED_RST_Pin, GPIO_PIN_RESET); // OLED off
+  extern SPI_HandleTypeDef OLED_SPI;
+  graphics.begin(&OLED_SPI);
+
+#ifdef USE_USB_HOST
+  // enable USB Host power
+  HAL_GPIO_WritePin(USB_HOST_PWR_EN_GPIO_Port, USB_HOST_PWR_EN_Pin, GPIO_PIN_SET);
+#endif
+
+  owl.setup();
+}
+
+void loop(void){
+
+#ifdef USE_USB_HOST
+  if(HAL_GPIO_ReadPin(USB_HOST_PWR_FAULT_GPIO_Port, USB_HOST_PWR_FAULT_Pin) == GPIO_PIN_RESET){
+    if(HAL_GPIO_ReadPin(USB_HOST_PWR_EN_GPIO_Port, USB_HOST_PWR_EN_Pin) == GPIO_PIN_SET){
+      HAL_GPIO_WritePin(USB_HOST_PWR_EN_GPIO_Port, USB_HOST_PWR_EN_Pin, GPIO_PIN_RESET);
+      error(USB_ERROR, "USBH PWR Fault");
+    }
+  }else{
+    MX_USB_HOST_Process();
+  }
+#endif
+
+#ifdef USE_SCREEN
+  graphics.draw();
+  graphics.display();
+#endif /* USE_SCREEN */
+
+  owl.loop();
+
+  if(updateMAX11300){
+    MAX11300_setDeviceControl(DCR_DACCTL_ImmUpdate|DCR_DACREF_Int|DCR_ADCCTL_ContSweep /* |DCR_ADCCONV_200ksps|DCR_BRST_Contextual*/);
+    for(int i=0; i<20; ++i){
+      uint16_t mode;
+      switch(portMode[i]){
+      case PORT_UNI_OUTPUT:
+	mode = PCR_Range_DAC_0_P10|PCR_Mode_DAC;
+	break;
+      case PORT_UNI_INPUT:
+      default:
+	mode = PCR_Range_ADC_0_P10|PCR_Mode_ADC_SgEn_PosIn|PCR_ADCSamples_16|PCR_ADCref_INT;
+	break;
+      }
+      MAX11300_setPortMode(i+1, mode);
+    }
+    updateMAX11300 = false;
+  }
+  TLC5946_Refresh_GS();
+  Encoders_readAll();
+  graphics.params.updateEncoders(Encoders_get(), 7);
+  MAX11300_bulkreadADC();
+  for(int i=0; i<16; ++i){
+    if(getPortMode(i) == PORT_UNI_INPUT){
+      graphics.params.updateValue(i, MAX11300_getADCValue(i+1));
+      uint16_t val = graphics.params.parameters[i]>>2;
+      setLed(i, dyn_rainbowinputs[val&0x3ff]);
+    }else{
+      // DACs
+    // TODO: store values set from patch somewhere and multiply with user[] value for outputs
+    // graphics.params.updateOutput(i, getOutputValue(i));
+      // MAX11300_setDACValue(i+1, graphics.params.parameters[i]);
+      graphics.params.updateValue(i, 0);
+      uint16_t val = graphics.params.parameters[i]>>2;
+      setLed(i, dyn_rainbowoutputs[val&0x3ff]);
+      MAX11300_setDAC(i+1, graphics.params.parameters[i]);
+    }
+  }
+  for(int i=16; i<20; ++i){
+    if(getPortMode(i) == PORT_UNI_INPUT){
+      graphics.params.updateValue(i, MAX11300_getADCValue(i+1));
+    }else{
+      graphics.params.updateValue(i, 0);
+      MAX11300_setDAC(i+1, graphics.params.parameters[i]);
+    }
+  }
+  // MAX11300_bulkwriteDAC();
+}
