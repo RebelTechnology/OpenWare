e2c216ad6a399d8a636a510204620ceddaec6a6a
Martin Klang <martin@rebel-it.com>
2021-04-22 17:58:44 +0200

    updated for new pin mappings and direct button LED control

--- Witch/Src/Witch.cpp
+++ Witch/Src/Witch.cpp
@@ -10,6 +10,7 @@
 #include "Codec.h"
 #include "ApplicationSettings.h"
 #include "qint.h"
+#include "Pin.h"
 
 #ifndef min
 #define min(a,b) ((a)<(b)?(a):(b))
@@ -20,6 +21,27 @@
 #ifndef abs
 #define abs(x) ((x)>0?(x):-(x))
 #endif
+
+// LEDs
+// 1, 2, 3, 4: CV level A, B, C, D
+// 5, 6: DAC out F, G
+// 7, 8, 9, 10: LED buttons
+
+// Buttons
+// SW1, SW2, SW3 : exti
+// SW4, SW5/mode: poll
+
+Pin ledpwm(LEDPWM_GPIO_Port, LEDPWM_Pin);
+Pin led7(LEDSW1_GPIO_Port, LEDSW1_Pin);
+Pin led8(LEDSW2_GPIO_Port, LEDSW2_Pin);
+Pin led9(LEDSW3_GPIO_Port, LEDSW3_Pin);
+Pin led10(LEDSW4_GPIO_Port, LEDSW4_Pin);
+
+Pin sw1(SW1_GPIO_Port, SW1_Pin);
+Pin sw2(SW2_GPIO_Port, SW2_Pin);
+Pin sw3(SW3_GPIO_Port, SW3_Pin);
+Pin sw4(SW4_GPIO_Port, SW4_Pin);
+Pin sw5(SW5_GPIO_Port, SW5_Pin);
 
 template<size_t SIZE, typename value_t>
 class TakeoverControls {
@@ -62,24 +84,34 @@ int16_t dac_values[2] = {0, 0};
 void onChangePin(uint16_t pin){
   if(owl.getOperationMode() == RUN_MODE){
     switch(pin){
+    case SW1_Pin:
+      {
+	bool state = HAL_GPIO_ReadPin(SW1_GPIO_Port, SW1_Pin) == GPIO_PIN_RESET;
+	setButtonValue(PUSHBUTTON, state);
+	setButtonValue(BUTTON_A, state);
+	setLed(7, state ? RED_COLOUR : NO_COLOUR);
+	break;
+      }
     case SW2_Pin:
       {
 	bool state = HAL_GPIO_ReadPin(SW2_GPIO_Port, SW2_Pin) == GPIO_PIN_RESET;
 	setButtonValue(BUTTON_B, state);
+	setLed(8, state ? RED_COLOUR : NO_COLOUR);
 	break;
       }
     case SW3_Pin:
       {
 	bool state = HAL_GPIO_ReadPin(SW3_GPIO_Port, SW3_Pin) == GPIO_PIN_RESET;
 	setButtonValue(BUTTON_C, state);
+	setLed(9, state ? RED_COLOUR : NO_COLOUR);
 	break;
       }
-    case SW4_Pin:
-      {
-	bool state = HAL_GPIO_ReadPin(SW4_GPIO_Port, SW4_Pin) == GPIO_PIN_RESET;
-	setButtonValue(BUTTON_D, state);
-	break;
-      }
+    // case SW4_Pin:
+    //   {
+    // 	bool state = HAL_GPIO_ReadPin(SW4_GPIO_Port, SW4_Pin) == GPIO_PIN_RESET;
+    // 	setButtonValue(BUTTON_D, state);
+    // 	break;
+    //   }
     }
   }
 }
@@ -104,19 +136,18 @@ void setAnalogValue(uint8_t ch, int16_t value){
 void setGateValue(uint8_t ch, int16_t value){
   if(owl.getOperationMode() == RUN_MODE){
     switch(ch){
-      // todo: always fiddle pushbutton LEDs to allow toggle
-      // case BUTTON_A:
-      //   setLed(1, value);
-      //   break;
-      // case BUTTON_B:
-      //   setLed(2, value);
-      //   break;
-      // case BUTTON_C:
-      //   setLed(3, value);
-      //   break;
-      // case BUTTON_D:
-      //   setLed(f, value);
-      //   break;
+    case BUTTON_A:
+      setLed(7, value ? RED_COLOUR : NO_COLOUR);
+      break;
+    case BUTTON_B:
+      setLed(8, value ? RED_COLOUR : NO_COLOUR);
+      break;
+    case BUTTON_C:
+      setLed(9, value ? RED_COLOUR : NO_COLOUR);
+      break;
+    case BUTTON_D:
+      setLed(10, value ? RED_COLOUR : NO_COLOUR);
+      break;
     case PUSHBUTTON:
     case BUTTON_E:
       HAL_GPIO_WritePin(TR_OUT1_GPIO_Port, TR_OUT1_Pin, value ? GPIO_PIN_RESET :  GPIO_PIN_SET);
@@ -168,11 +199,55 @@ void setLed(uint8_t led, uint32_t rgb){
   case 6:
     TIM1->CCR1 = value;
     break;
+  case 7:
+    if(rgb == RED_COLOUR){
+      ledpwm.high();
+      led7.low();
+    }else if(rgb == YELLOW_COLOUR){
+      ledpwm.low();
+      led7.high();
+    }else{
+      led7.set(ledpwm.get());
+    }
+    break;
+  case 8:
+    if(rgb == RED_COLOUR){
+      ledpwm.high();
+      led8.low();
+    }else if(rgb == YELLOW_COLOUR){
+      ledpwm.low();
+      led8.high();
+    }else{
+      led8.set(ledpwm.get());
+    }
+    break;
+  case 9:
+    if(rgb == RED_COLOUR){
+      ledpwm.high();
+      led9.low();
+    }else if(rgb == YELLOW_COLOUR){
+      ledpwm.low();
+      led9.high();
+    }else{
+      led9.set(ledpwm.get());
+    }
+    break;
+  case 10:
+    if(rgb == RED_COLOUR){
+      ledpwm.high();
+      led10.low();
+    }else if(rgb == YELLOW_COLOUR){
+      ledpwm.low();
+      led10.high();
+    }else{
+      led10.set(ledpwm.get());
+    }
+    break;
   }
 }
 
 bool isModeButtonPressed(){
-  return HAL_GPIO_ReadPin(SW5_GPIO_Port, SW5_Pin) == GPIO_PIN_RESET;
+  return !sw5.get(); // HAL_GPIO_ReadPin(SW5_GPIO_Port, SW5_Pin) == GPIO_PIN_RESET;
 }
 
 int16_t getAttenuatedCV(uint8_t index, uint16_t* adc_values){
@@ -225,125 +300,6 @@ void updateParameters(int16_t* parameter_values, size_t parameter_len, uint16_t*
   }
 }
 
-
-/**
-  * @brief  Configure gpio mode for a dedicated pin on dedicated port.
-  * @note   I/O mode can be Input mode, General purpose output, Alternate function mode or Analog.
-  * @note   Warning: only one pin can be passed as parameter.
-  * @rmtoll MODER        MODEy         LL_GPIO_SetPinMode
-  * @param  GPIOx GPIO Port
-  * @param  Pin This parameter can be one of the following values:
-  *         @arg @ref LL_GPIO_PIN_0
-  *         @arg @ref LL_GPIO_PIN_1
-  *         @arg @ref LL_GPIO_PIN_2
-  *         @arg @ref LL_GPIO_PIN_3
-  *         @arg @ref LL_GPIO_PIN_4
-  *         @arg @ref LL_GPIO_PIN_5
-  *         @arg @ref LL_GPIO_PIN_6
-  *         @arg @ref LL_GPIO_PIN_7
-  *         @arg @ref LL_GPIO_PIN_8
-  *         @arg @ref LL_GPIO_PIN_9
-  *         @arg @ref LL_GPIO_PIN_10
-  *         @arg @ref LL_GPIO_PIN_11
-  *         @arg @ref LL_GPIO_PIN_12
-  *         @arg @ref LL_GPIO_PIN_13
-  *         @arg @ref LL_GPIO_PIN_14
-  *         @arg @ref LL_GPIO_PIN_15
-  * @param  Mode This parameter can be one of the following values:
-  *         @arg @ref LL_GPIO_MODE_INPUT
-  *         @arg @ref LL_GPIO_MODE_OUTPUT
-  *         @arg @ref LL_GPIO_MODE_ALTERNATE
-  *         @arg @ref LL_GPIO_MODE_ANALOG
-  * @retval None
-  */
-#define LL_GPIO_PIN_0                      GPIO_BSRR_BS_0 /*!< Select pin 0 */
-#define LL_GPIO_PIN_1                      GPIO_BSRR_BS_1 /*!< Select pin 1 */
-#define LL_GPIO_PIN_2                      GPIO_BSRR_BS_2 /*!< Select pin 2 */
-#define LL_GPIO_PIN_3                      GPIO_BSRR_BS_3 /*!< Select pin 3 */
-#define LL_GPIO_PIN_4                      GPIO_BSRR_BS_4 /*!< Select pin 4 */
-#define LL_GPIO_PIN_5                      GPIO_BSRR_BS_5 /*!< Select pin 5 */
-#define LL_GPIO_PIN_6                      GPIO_BSRR_BS_6 /*!< Select pin 6 */
-#define LL_GPIO_PIN_7                      GPIO_BSRR_BS_7 /*!< Select pin 7 */
-#define LL_GPIO_PIN_8                      GPIO_BSRR_BS_8 /*!< Select pin 8 */
-#define LL_GPIO_PIN_9                      GPIO_BSRR_BS_9 /*!< Select pin 9 */
-#define LL_GPIO_PIN_10                     GPIO_BSRR_BS_10 /*!< Select pin 10 */
-#define LL_GPIO_PIN_11                     GPIO_BSRR_BS_11 /*!< Select pin 11 */
-#define LL_GPIO_PIN_12                     GPIO_BSRR_BS_12 /*!< Select pin 12 */
-#define LL_GPIO_PIN_13                     GPIO_BSRR_BS_13 /*!< Select pin 13 */
-#define LL_GPIO_PIN_14                     GPIO_BSRR_BS_14 /*!< Select pin 14 */
-#define LL_GPIO_PIN_15                     GPIO_BSRR_BS_15 /*!< Select pin 15 */
-
-#define LL_GPIO_MODE_INPUT                 (0x00000000U) /*!< Select input mode */
-#define LL_GPIO_MODE_OUTPUT                GPIO_MODER_MODER0_0  /*!< Select output mode */
-#define LL_GPIO_MODE_ALTERNATE             GPIO_MODER_MODER0_1  /*!< Select alternate function mode */
-#define LL_GPIO_MODE_ANALOG                GPIO_MODER_MODER0    /*!< Select analog mode */
-
-__STATIC_INLINE void LL_GPIO_SetPinMode(GPIO_TypeDef *GPIOx, uint32_t Pin, uint32_t Mode)
-{
-  MODIFY_REG(GPIOx->MODER, (GPIO_MODER_MODER0 << (POSITION_VAL(Pin) * 2U)), (Mode << (POSITION_VAL(Pin) * 2U)));
-}
-__STATIC_INLINE uint32_t LL_GPIO_IsInputPinSet(GPIO_TypeDef *GPIOx, uint32_t PinMask)
-{
-  return (READ_BIT(GPIOx->IDR, PinMask) == (PinMask));
-}
-__STATIC_INLINE uint32_t LL_GPIO_ReadInputPort(GPIO_TypeDef *GPIOx)
-{
-  return (uint32_t)(READ_REG(GPIOx->IDR));
-}
-__STATIC_INLINE uint32_t LL_GPIO_ReadOutputPort(GPIO_TypeDef *GPIOx)
-{
-  return (uint32_t)(READ_REG(GPIOx->ODR));
-}
-__STATIC_INLINE void LL_GPIO_WriteOutputPort(GPIO_TypeDef *GPIOx, uint32_t PortValue)
-{
-  WRITE_REG(GPIOx->ODR, PortValue);
-}
-
-#define SW1_LL_Pin LL_GPIO_PIN_2
-#define SW2_LL_Pin LL_GPIO_PIN_1
-#define SW3_LL_Pin LL_GPIO_PIN_2
-#define SW4_LL_Pin LL_GPIO_PIN_9
-
-bool fiddle(int i, bool selected){
-  GPIO_TypeDef* port;
-  uint16_t pin;
-  uint32_t llpin;
-  switch(i){
-  case 1: // PB2
-    port = SW1_GPIO_Port;
-    pin = SW1_Pin;
-    llpin = SW1_LL_Pin;
-    break;
-  case 2: // PB1
-    port = SW2_GPIO_Port;
-    pin = SW2_Pin;
-    llpin = SW2_LL_Pin;
-    break;
-  case 3: // PD2
-    port = SW3_GPIO_Port;
-    pin = SW3_Pin;
-    llpin = SW3_LL_Pin;
-    break;
-  case 4: // PB9
-    port = SW4_GPIO_Port;
-    pin = SW4_Pin;
-    llpin = SW4_LL_Pin;
-    break;
-  }
-  LL_GPIO_SetPinMode(port, llpin, LL_GPIO_MODE_INPUT);
-  // bool value = LL_GPIO_IsInputPinSet(port, llpin);
-  bool value = HAL_GPIO_ReadPin(port, pin) == GPIO_PIN_RESET;
-  // uint32_t portvalues = LL_GPIO_ReadOutputPort(port);
-  if(selected){
-    LL_GPIO_SetPinMode(port, llpin, LL_GPIO_MODE_OUTPUT);
-    // LL_GPIO_WriteOutputPort(port, portvalues & ~llpin); // switch pin off
-    // LL_GPIO_WriteOutputPort(port, port | llpin); // switch pin on
-    HAL_GPIO_WritePin(port, pin, GPIO_PIN_RESET);
-  }
-  return value;
-
-}
-
 #define PATCH_RESET_COUNTER (600/MAIN_LOOP_SLEEP_MS)
 
 static uint32_t counter = 0;
@@ -369,22 +325,29 @@ static void update_preset(){
       owl.setOperationMode(CONFIGURE_MODE);
     }else if(getErrorStatus() != NO_ERROR){
       owl.setOperationMode(ERROR_MODE);
-    }else{
-      // setLed(1, Q15_MUL_Q15(getAnalogValue(ADC_A), takeover.get(5)));
-      // setLed(2, Q15_MUL_Q15(getAnalogValue(ADC_C), takeover.get(6)));
-      // setLed(3, Q15_MUL_Q15(getAnalogValue(ADC_E), takeover.get(7)));
-      // setLed(4, Q15_MUL_Q15(getAnalogValue(ADC_G), takeover.get(8)));
     }
     break;
   case CONFIGURE_MODE:
     if(isModeButtonPressed()){
       uint8_t patchselect = program.getProgramIndex();
-      for(int i=1; i<=4; ++i){
-	if(fiddle(i, i==patchselect))
-	  patchselect = i;
-      }
+      if(!sw1.get())
+      	patchselect = 1;
+      else if(!sw2.get())
+      	patchselect = 2;
+      else if(!sw3.get())
+      	patchselect = 3;
+      else if(!sw4.get())
+      	patchselect = 4;
       if(patchselect >= registry.getNumberOfPatches())
 	patchselect = program.getProgramIndex();
+      for(int i=0; i<4; ++i){
+	// if(getButtonValue(BUTTON_A+i))
+	//   patchselect = i+1;
+	if(patchselect == i+1)
+	  setLed(7+i, YELLOW_COLOUR);
+	else
+	  setLed(7+i, NO_COLOUR);
+      }
       if(program.getProgramIndex() != patchselect){
 	program.loadProgram(patchselect); // enters load mode
 	program.resetProgram(false);
@@ -397,8 +360,6 @@ static void update_preset(){
 	codec.setOutputGain(value);
       }
     }else{
-      for(int i=1; i<=4; ++i)
-	fiddle(i, false); // set all to input mode
       takeover.reset(false);
       owl.setOperationMode(RUN_MODE);
       // reset CV outputs to previous values
@@ -415,8 +376,7 @@ static void update_preset(){
     setLed(4, counter > PATCH_RESET_COUNTER*0.5 ? 4095 : 0);
     if(isModeButtonPressed()){
       setErrorStatus(NO_ERROR);
-      owl.setOperationMode(RUN_MODE); // allows new patch selection if patch doesn't load
-      program.resetProgram(false);
+      owl.setOperationMode(CONFIGURE_MODE);
     }
     break;
   }
@@ -425,8 +385,9 @@ static void update_preset(){
 }
 
 void onChangeMode(OperationMode new_mode, OperationMode old_mode){
-  for(int i=1; i<=6; ++i)
-    setLed(i, 0);
+  ledpwm.high(); // switch button leds to red
+  for(int i=1; i<=10; ++i)
+    setLed(i, NO_COLOUR);
   setGateValue(BUTTON_E, 0); // this will only have an effect in RUN mode
   setGateValue(BUTTON_F, 0);
   counter = 0;
@@ -436,19 +397,17 @@ void setup(){
   initLed();
   HAL_GPIO_WritePin(LEDPWM_GPIO_Port, LEDPWM_Pin, GPIO_PIN_SET);
   owl.setup();
-  for(size_t i=5; i<9; ++i)
+   for(size_t i=5; i<9; ++i)
     takeover.set(i, 2048); // set CV attenuation to 1
   takeover.set(9, settings.audio_output_gain<<5);
 }
 
 void loop(void){
   MX_USB_HOST_Process(); // todo: enable PWR management
-  if(owl.getOperationMode() == RUN_MODE){
-    bool state = HAL_GPIO_ReadPin(SW1_GPIO_Port, SW1_Pin) == GPIO_PIN_RESET;
-    if(state != getButtonValue(BUTTON_A)){
-      setButtonValue(PUSHBUTTON, state);
-      setButtonValue(BUTTON_A, state);
-    }
+  bool state = !sw4.get(); // HAL_GPIO_ReadPin(SW4_GPIO_Port, SW4_Pin) == GPIO_PIN_RESET;
+  if(state != getButtonValue(BUTTON_D)){
+    setButtonValue(BUTTON_D, state);
+    setLed(10, state ? RED_COLOUR : NO_COLOUR);
   }
   update_preset();
   owl.loop();
