2c45445156b3c4fd1253ecaf666f910002490b3d
Martin Klang <martin@rebel-it.com>
2021-08-28 11:56:11 +0200

    enable/disable hpf on CS4271

--- Source/Codec.cpp
+++ Source/Codec.cpp
@@ -290,6 +290,12 @@ void Codec::setHighPass(bool hpf){
   else
     codec_write(82, 0b00000111); // disable HPF for all ADC channels
 #endif
+#ifdef USE_CS4271
+  if(hpf)
+    codec_write(0x06, 0x10); // hp filters enabled
+  else
+    codec_write(0x06, 0x10 | 0x03 ); // hp filters disabled
+#endif
 }
 
 #ifdef USE_IIS3DWB
