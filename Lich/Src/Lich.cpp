e2f7b328340c5f1deea237872bad77c74c1a4a82
Martin Klang <martin@rebel-it.com>
2021-12-08 11:21:16 +0100

    OperationMode changes

--- Lich/Src/Lich.cpp
+++ Lich/Src/Lich.cpp
@@ -156,7 +156,7 @@ void setProgress(uint16_t value, const char* msg){
   progress = value;
 }
 
-void onChangeMode(OperationMode new_mode, OperationMode old_mode){
+void onChangeMode(uint8_t new_mode, uint8_t old_mode){
   progress = 0;
   patchselect = program.getProgramIndex();
   setEncoderValue(patchselect);
