968f853416478755117b6b6a35e0f31f6bec2ccc
Martin Klang <martin@rebel-it.com>
2021-12-17 16:31:31 +0100

    changed to support 5 USBD interfaces instead of 1

--- Xibeca/USB_DEVICE/Target/usbd_conf.h
+++ Xibeca/USB_DEVICE/Target/usbd_conf.h
@@ -64,7 +64,7 @@
   */
 
 /*---------- -----------*/
-#define USBD_MAX_NUM_INTERFACES     1U
+#define USBD_MAX_NUM_INTERFACES     5U
 /*---------- -----------*/
 #define USBD_MAX_NUM_CONFIGURATION     1U
 /*---------- -----------*/
